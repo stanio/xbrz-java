8a5e585c2e00176825440f80582f7a41412e0559
Stanimir Stamenkov <stanio@yahoo.com>
2023-04-09 13:48:14 +0300

    Update icons demo
    Use original hires variants.

--- xbrz-awt/src/test/java/io/github/stanio/xbrz/awt/demo/IconsDemo.java
+++ xbrz-awt/src/test/java/io/github/stanio/xbrz/awt/demo/IconsDemo.java
@@ -4,29 +4,38 @@
  */
 package io.github.stanio.xbrz.awt.demo;
 
-import static java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR;
+import static java.awt.RenderingHints.VALUE_INTERPOLATION_BICUBIC;
 import static java.awt.RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR;
 
 import java.util.function.BiFunction;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 import io.github.stanio.xbrz.awt.AwtXbrz;
-import io.github.stanio.xbrz.awt.XbrzImage;
 import io.github.stanio.xbrz.awt.util.MultiResolutionCachedImage;
 
 @SuppressWarnings("serial")
@@ -35,6 +44,14 @@ public class IconsDemo extends JFrame {
     public IconsDemo() {
         super("Icon Scaling");
         super.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        super.getRootPane().getActionMap().put("exit", new AbstractAction() {
+            @Override public void actionPerformed(ActionEvent event) {
+                dispatchEvent(new WindowEvent(IconsDemo.this, WindowEvent.WINDOW_CLOSING));
+                dispose();
+            }
+        });
+        super.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+                           .put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false), "exit");
         initUI();
     }
 
@@ -43,25 +60,33 @@ public class IconsDemo extends JFrame {
     }
 
     private void initUI() {
-        Image baseImage = loadImage("editbookmarks.png");
+        final String imageName = "editbookmarks2";
+        Image loresImage = loadImage(imageName + "-16.png");
+        Image hiresImage = loadImage(imageName + "-32.png");
 
         JComponent content = Box.createVerticalBox();
-        content.add(createIcons("Default", (w, h) -> baseImage));
-        content.add(createIcons("SCALE_AREA_AVERAGING",
-                (w, h) -> baseImage.getScaledInstance(w, h, Image.SCALE_AREA_AVERAGING)));
-        content.add(createIcons("CUSTOM_AREA_AVERAGING", scaleAverage(baseImage)));
-        content.add(createIcons("INTERPOLATION_BILINEAR",
-                (w, h) -> scaleImage(baseImage, w, h, VALUE_INTERPOLATION_BILINEAR)));
-        content.add(createIcons("xBRZ", scaleBRZ(baseImage)));
+        content.setBackground(new javax.swing.JToolBar().getBackground());
+        content.setOpaque(true);
 
-        Image hiresImage = XbrzImage.mrImage(baseImage).getResolutionVariant(32, 32);
+        content.add(createIcons("Default (nearest-neighbor)", (w, h) -> loresImage));
+        // https://bugs.openjdk.org/browse/JDK-8243061 randomly results in
+        // java.lang.ClassCastException: class [I cannot be cast to class [B
+        content.add(createIcons("SCALE_AREA_AVERAGING",
+                (w, h) -> loresImage.getScaledInstance(w, h, Image.SCALE_AREA_AVERAGING)));
+        //content.add(createIcons("CUSTOM_AREA_AVERAGING", scaleAverage(baseImage)));
+        content.add(createIcons("xBRZ", scaleBRZ(loresImage)));
+        content.add(createIcons("INTERPOLATION_BICUBIC",
+                (w, h) -> scaleImage(loresImage, w, h, VALUE_INTERPOLATION_BICUBIC)));
+
         content.add(createIcons2("Default (hires base)", (w, h) -> hiresImage));
+        // https://bugs.openjdk.org/browse/JDK-8243061 randomly results in
+        // java.lang.ClassCastException: class [I cannot be cast to class [B
         content.add(createIcons2("SCALE_AREA_AVERAGING (hires base)",
                 (w, h) -> hiresImage.getScaledInstance(w, h, Image.SCALE_AREA_AVERAGING)));
-        content.add(createIcons2("CUSTOM_AREA_AVERAGING (hires base)", scaleAverage(hiresImage)));
+        //content.add(createIcons2("CUSTOM_AREA_AVERAGING (hires base)", scaleAverage(hiresImage)));
         content.add(createIcons2("xBRZ (hires base)", scaleBRZ(hiresImage)));
-        content.add(createIcons2("INTERPOLATION_BILINEAR (hires base)",
-                (w, h) -> scaleImage(hiresImage, w, h, VALUE_INTERPOLATION_BILINEAR)));
+        content.add(createIcons2("INTERPOLATION_BICUBIC (hires base)",
+                (w, h) -> scaleImage(hiresImage, w, h, VALUE_INTERPOLATION_BICUBIC)));
 
         content.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         super.add(content);
@@ -80,23 +105,27 @@ public class IconsDemo extends JFrame {
     private static JComponent createIcons(String label,
             BiFunction<Integer, Integer, Image> scaler, int hiliteSize) {
         JComponent comp = createHorizontalBox();
+        comp.setOpaque(false);
+        comp.setAlignmentX(Component.LEFT_ALIGNMENT);
         for (int size : new int[] { 16, 20, 24, 28, 32, 36, 40, 44, 48 }) {
             JLabel icon = new JLabel(new ImageIcon(
                     MultiResolutionCachedImage.of(size, size, scaler)));
-            comp.add(icon);
-            comp.add(Box.createHorizontalStrut(5));
+            comp.add(icon, getHorizontalConstraints());
+            comp.add(Box.createHorizontalStrut(10), getHorizontalConstraints());
             if (size == hiliteSize) {
-                icon.setBackground(new Color(0, 0xFF, 0, 0x80));
+                icon.setBackground(new Color(0xFF, 0xFF, 0xFF, 0x80));
                 icon.setOpaque(true);
             }
         }
-        comp.add(new JLabel(label));
-        comp.add(Box.createHorizontalGlue());
+        JLabel caption = new JLabel(label);
+        caption.setFont(Font.decode("Segoe UI-18"));
+        comp.add(caption, getHorizontalConstraints());
+        comp.add(Box.createHorizontalGlue(), getHorizontalConstraints(1.0));
         return comp;
     }
 
     private static JComponent createHorizontalBox() {
-        return new Box(BoxLayout.LINE_AXIS) {
+        return new JPanel(new GridBagLayout()) {
             @Override
             public Dimension getMaximumSize() {
                 Dimension size = super.getMaximumSize();
@@ -106,11 +135,21 @@ public class IconsDemo extends JFrame {
         };
     }
 
-    private static BiFunction<Integer, Integer, Image> scaleAverage(Image baseImage) {
+    private static GridBagConstraints getHorizontalConstraints() {
+        return getHorizontalConstraints(0);
+    }
+
+    private static GridBagConstraints getHorizontalConstraints(double weightx) {
+        return new GridBagConstraints(GridBagConstraints.RELATIVE, 0, 1, 1, weightx, 0,
+                                      GridBagConstraints.CENTER, GridBagConstraints.VERTICAL,
+                                      new Insets(0, 0, 1, 0), 2, 1);
+    }
+
+    static BiFunction<Integer, Integer, Image> scaleAverage(Image baseImage) {
         return (w, h) -> {
             Image integralBase = upscaleIntegral(baseImage, w, h,
                     (iw, ih) -> scaleImage(baseImage, iw, ih, VALUE_INTERPOLATION_NEAREST_NEIGHBOR));
-            return scaleImage(integralBase, w, h, VALUE_INTERPOLATION_BILINEAR);
+            return scaleImage(integralBase, w, h, VALUE_INTERPOLATION_BICUBIC);
         };
     }
 
@@ -118,7 +157,7 @@ public class IconsDemo extends JFrame {
         return (w, h) -> {
             Image integralBase = upscaleIntegral(baseImage, w, h,
                     (iw, ih) -> AwtXbrz.scaleImage(baseImage, iw, ih));
-            return scaleImage(integralBase, w, h, VALUE_INTERPOLATION_BILINEAR);
+            return scaleImage(integralBase, w, h, VALUE_INTERPOLATION_BICUBIC);
         };
     }
 
@@ -154,7 +193,13 @@ public class IconsDemo extends JFrame {
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            try {
+                javax.swing.UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (Exception e) {
+                System.err.println(e);
+            }
             IconsDemo frame = new IconsDemo();
+            frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
             frame.pack();
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
